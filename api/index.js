c8c55d2e280fc6f4a47f775d4e5a7f5197c2e235
Saumya Gupta <gsaumya333@gmail.com>
2024-09-06 13:44:33 +0530

    create a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
@@ -18,3 +18,13 @@ app.listen(3000,()=>{
 );
 app.use("/api/user",userRouter);
 app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||'Internal Server error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
