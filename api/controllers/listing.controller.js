cccf85c33c91fb5ac4547ee319ea8cee5b2a7d06
saumyag25 <gsaumya333@gmail.com>
2024-09-25 23:50:37 +0530

    create update api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -25,3 +25,18 @@ export const deleteListing=async(req,res,next)=>{
       next(error);
    }
 }
+export const updateListing=async(req,res,next)=>{
+   const listing=await Listing.findById(req.params.id);
+   if(!listing){
+      return next(errorHandler(404,"Listing not found"))
+      }
+   if(req.user.id!==listing.userRef){
+         return next(errorHandler(401,"You are not authorized to update this listing"))
+         }
+   try {
+      const updatedListing=await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true})
+      res.status(200).json(updatedListing)
+   } catch (error) {
+      next(error);
+   }
+}
