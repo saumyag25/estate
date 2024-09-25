737049a26f40f9761cf86008107ff3aa653c9a81
saumyag25 <gsaumya333@gmail.com>
2024-09-25 23:33:34 +0530

    complete user delete functionality and show listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,8 @@ export default function Profile() {
   const [updateSuccess,setUpdateSuccess]=useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showListingsError,setShowListingsError]=useState(false);
+  const [userListings,setUserListings]=useState([]);
   console.log(formData)
   console.log(filePerc)
   console.log(file);
@@ -104,6 +106,35 @@ export default function Profile() {
            dispatch(deleteUserFailure(error.message))
          }
   };
+  const handleShowListings=async()=>{
+    try {
+      setShowListingsError(false)
+      const res=await fetch(`/api/user/listings/${currentUser._id}`);
+      const data= await res.json();
+      if(data.success===false){
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
+  const handleListingDelete=async(listingId)=>{
+    try {
+      const res=await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      })
+      const data=await res.json();
+      if(data.success===false){
+        console.log(data.message)
+        return;
+      }
+      setUserListings((prev)=> prev.filter((listing)=>listing._id!==listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'> 
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -142,6 +173,31 @@ export default function Profile() {
         <span onClick={handleSignOut} className='text-red -700 cursor-pointer'>Sign Out</span>
        </div>
        <p className='text-green-700 mt-5'>{updateSuccess?'User is updated sucessfully':''}</p>
+       <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+       <p className='text-red-700 mt-5'>{showListingsError?'Errorshowing listings':''}</p>
+
+
+       {userListings && userListings.length>0 &&(
+       <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+        {userListings.map((listing)=>(
+        <div key={listing._id} className='border rounded-lg  p-3 flex justify-between items-center gap-4'>
+          <Link to= {`/listing/${listing._id}`}>
+          <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain' />
+          </Link>
+          <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+          <p  >{listing.name}</p>
+          </Link>
+          <div className='flex flex-col items-center'>
+            <button onClick={()=>handleListingDelete(listing._id)} className=' text-red-700 uppercase'>Delete</button>
+            <button className='text-green-700 uppercase'> Edit</button>
+            </div>
+          </div>
+       ))}
+        </div>
+        )}
     </div>
   ); 
 }
